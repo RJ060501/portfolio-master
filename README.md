6c44e2d1a255ce60841031c107002600b513508f
RJ060501 <rjrussell605@gmail.com>
2022-09-09 13:03:13 -0600

    Changing Readme.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h2 align="center">
   Portfolio Website - v2.0<br/>
-  <a href="http://soumya-jit.tech/" target="_blank">soumyajit.tech</a>
+  <!-- <a href="http://soumya-jit.tech/" target="_blank">soumyajit.tech</a> -->
 </h2>
 <div align="center">
   <img alt="Demo" src="./Images/newreadme-img1.png" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/RJ060501"
                 target="_blank"
                 className="fork-btn-inner"
               >
