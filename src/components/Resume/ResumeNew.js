cd0a57a30d13f6c984851b6be7aaad6c4d9c98fc
RJ060501 <rjrussell605@gmail.com>
2025-01-24 08:21:43 -0700

    Updated Resume...

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,8 +9,6 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import RRResume from "../../Assets/RRResume.pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// const resumeLink =
-//   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
